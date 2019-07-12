8e589aa2ba622a6d4afc78217cc3b93830b0df18
saneto <aymanezaircha@gmail.com>
2019-07-12 16:37:03 +0200

    commit evol

---
+++ build/precache-manifest.15d91aacf7481b3c70bc747976f87356.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edcec7bbd1736ce5ac40a37e54941e41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01da25d65064db80ebdb",
+    "url": "/static/css/main.bc40163f.chunk.css"
+  },
+  {
+    "revision": "dedcc181e5c3003f299a",
+    "url": "/static/js/2.cf2053f0.chunk.js"
+  },
+  {
+    "revision": "01da25d65064db80ebdb",
+    "url": "/static/js/main.2bf10807.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,41 +5,22 @@ import {Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 import SignOutButton from '../SignOut';
 import { AuthUserContext } from '../Session';
+import * as ROLES from '../../constants/roles';
 
 const Navigation = () => (
-  <AuthUserContext.Consumer>
-    {
-      authUser => authUser ? (
+  <div>
+    <AuthUserContext.Consumer>
+    {authUser =>
+      authUser ? (
         <NavigationAuth authUser={authUser} />
       ) : (
-        <NavigationAuth />
+        <NavigationNonAuth />
       )
     }
-  </AuthUserContext.Consumer>
+    </AuthUserContext.Consumer>
+  </div>
 );
-  
-  /*const NavigationAuth  = ({ authUser }) => (
-    <ul>
-      <li>
-        <Link to={ROUTES.LANDING}>Landing</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.HOME}>Home</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ACCOUNT}>Account</Link>
-      </li>
-      {!!authUser.roles[ROLES.ADMIN] && (
-        <li>
-          <Link to={ROUTES.ADMIN}>Admin</Link>
-        </li>
-      )}
-      <li>
-        <SignOutButton />
-      </li>
-    </ul>
-  );*/
-  const NavigationAuth  = () => (
+  const NavigationAuth  = ({ authUser }) => (
     <ul>
       <li>
         <Link to={ROUTES.LANDING}>Landing</Link>
@@ -51,15 +32,32 @@ const Navigation = () => (
         <Link to={ROUTES.ACCOUNT}>Account</Link>
       </li>
       
+      {!!authUser.roles[ROLES.ADMIN] && (
         <li>
           <Link to={ROUTES.ADMIN}>Admin</Link>
         </li>
+      )}
 
       <li>
         <SignOutButton />
       </li>
     </ul>
   );
+
+  const NavigationNonAuth = () => (
+    <ul>
+      <li>
+        <Link to={ROUTES.LANDING}>Landing</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+      </li>
+      <li>
+      <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+    </li>
+    </ul>
+  );
+  
   
   
 
